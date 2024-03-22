4a8eec107f7e74390b07033d2212d2c157cd2876
Paul Thompson <pmt10@case.edu>
2024-03-22 08:59:55 -0400

    created analog render area with template from QT example

---
+++ spikesorter/AnalogRenderArea/analogrenderarea.hpp
@@ -0,0 +1,51 @@
+//
+// Created by wanglab on 3/22/2024.
+//
+// Copyright (C) 2016 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#ifndef SPIKESORTER_ANALOGRENDERAREA_HPP
+#define SPIKESORTER_ANALOGRENDERAREA_HPP
+
+#include <QBrush>
+#include <QPen>
+#include <QPixmap>
+#include <QWidget>
+
+//! [0]
+class AnalogRenderArea : public QWidget
+{
+Q_OBJECT
+
+public:
+    enum Shape { Line, Points, Polyline, Polygon, Rect, RoundedRect, Ellipse, Arc,
+        Chord, Pie, Path, Text, Pixmap };
+
+    explicit AnalogRenderArea(QWidget *parent = nullptr);
+
+    QSize minimumSizeHint() const override;
+    QSize sizeHint() const override;
+
+public slots:
+    void setShape(Shape shape);
+    void setPen(const QPen &pen);
+    void setBrush(const QBrush &brush);
+    void setAntialiased(bool antialiased);
+    void setTransformed(bool transformed);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    Shape shape;
+    QPen pen;
+    QBrush brush;
+    bool antialiased;
+    bool transformed;
+    QPixmap pixmap;
+};
+
+//! [0]
+
+
+#endif //SPIKESORTER_ANALOGRENDERAREA_HPP
