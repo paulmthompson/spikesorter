7659926af9da7b73136719e022a823c3748ed99d
Paul Thompson <pmt10@case.edu>
2024-03-27 14:14:56 -0400

    LSST formatting for private member variables

--- spikesorter/AnalogRenderArea/AnalogRenderXAxisProps.hpp
+++ spikesorter/AnalogRenderArea/AnalogRenderXAxisProps.hpp
@@ -6,73 +6,73 @@
 class AnalogRenderXAxisProps {
 public:
     AnalogRenderXAxisProps() {
-        sampling_rate = 30000.0;
-        n_samples_total = 0;
+        _sampling_rate = 30000.0;
+        _n_samples_total = 0;
 
-        center_sample = 0;
+        _center_sample = 0;
 
-        n_samples_to_show = 10000;
+        _n_samples_to_show = 10000;
         calculate_first_and_last_sample_to_show();
     }
     void setXAxisProps(float sampling_rate, int n_samples_total) {
-        this->n_samples_total = n_samples_total;
-        this->sampling_rate = sampling_rate;
+        _n_samples_total = n_samples_total;
+        _sampling_rate = sampling_rate;
 
         calculate_first_and_last_sample_to_show();
     };
     void setSamplesToShow(int64_t n_samples) {
-        n_samples_to_show = n_samples;
+        _n_samples_to_show = n_samples;
         calculate_first_and_last_sample_to_show();
     };
     void setCenterSample(int64_t sample) {
-        center_sample = sample;
+        _center_sample = sample;
         calculate_first_and_last_sample_to_show();
     };
     int64_t getCenterSample() {
-        return this->center_sample;
+        return _center_sample;
     }
     int64_t getSamplesToShow() {
-        return last_sample_to_show - first_sample_to_show;
+        return _last_sample_to_show - _first_sample_to_show;
     };
     int64_t getFirstSample() {
-        return this->first_sample_to_show;
+        return _first_sample_to_show;
     };
     float getSamplingRate() {
-        return this->sampling_rate;
+        return _sampling_rate;
     };
 private:
-    float sampling_rate;
-    int64_t n_samples_total;
+    float _sampling_rate;
+    int64_t _n_samples_total;
 
-    int64_t n_samples_to_show;
+    int64_t _n_samples_to_show;
 
-    int64_t center_sample;
-    int64_t first_sample_to_show;
-    int64_t last_sample_to_show;
+    int64_t _center_sample;
+    int64_t _first_sample_to_show;
+    int64_t _last_sample_to_show;
 
     void calculate_first_and_last_sample_to_show() {
 
-        if (this->n_samples_total == 0) {
+        if (_n_samples_total == 0) {
             return;
         }
 
-        int64_t left_edge_sample = center_sample - n_samples_to_show / 2;
-        int64_t right_edge_sample = center_sample + n_samples_to_show / 2 - 1;
+        int64_t left_edge_sample = _center_sample - _n_samples_to_show / 2;
+        int64_t right_edge_sample = _center_sample + _n_samples_to_show / 2 - 1;
 
         if (left_edge_sample < 0) {
             left_edge_sample = 0;
-            right_edge_sample = n_samples_to_show - 1;
+            right_edge_sample = _n_samples_to_show - 1;
         }
-        if (right_edge_sample >= n_samples_total) {
-            right_edge_sample = n_samples_total - 1;
-            left_edge_sample = right_edge_sample - n_samples_to_show;
+        if (right_edge_sample >= _n_samples_total) {
+            right_edge_sample = _n_samples_total - 1;
+            left_edge_sample = right_edge_sample - _n_samples_to_show;
             if (left_edge_sample < 0) {
                 left_edge_sample = 0;
             }
         }
 
-        first_sample_to_show = left_edge_sample;
-        last_sample_to_show = right_edge_sample;
+        _first_sample_to_show = left_edge_sample;
+        _last_sample_to_show = right_edge_sample;
 
     }
 };
--- spikesorter/AnalogRenderArea/AnalogRenderYAxisProps.hpp
+++ spikesorter/AnalogRenderArea/AnalogRenderYAxisProps.hpp
@@ -20,16 +20,16 @@
 class AnalogRenderYAxisProps {
 public:
     AnalogRenderYAxisProps() {
-        n_channels_total = 0;
-        scroll_bar_position = 1;
+        _n_channels_total = 0;
+        _scroll_bar_position = 1;
 
     };
 
     void setYAxisProps(int64_t n_channels_total, int64_t n_channels_display) {
-        this->n_channels_total = n_channels_total;
-        this->n_channels_to_show = n_channels_display;
+        _n_channels_total = n_channels_total;
+        _n_channels_to_show = n_channels_display;
 
-        this->per_channel_gain = std::vector<float>(n_channels_total, 0.0);
+        _per_channel_gain = std::vector<float>(n_channels_total, 0.0);
 
         calculate_line_offsets();
         calculate_channel_bounds();
@@ -45,38 +45,38 @@ public:
      * @param n_channels_to_display
      */
     void setChannelsToDisplay(int64_t n_channels_to_display) {
-        this->n_channels_to_show = n_channels_to_display;
+        _n_channels_to_show = n_channels_to_display;
         calculate_line_offsets();
         calculate_channel_bounds();
         calculate_which_channels_to_show();
     };
 
     float getLineOffset(int channel) {
-        return line_offsets[channel];
+        return _line_offsets[channel];
     };
     float getLowerChannelBound(int channel) {
-        return lower_channel_bounds[channel];
+        return _lower_channel_bounds[channel];
     }
     float getChannelGain(int data_channel) {
-        return this->per_channel_gain[data_channel];
+        return _per_channel_gain[data_channel];
     }
     int64_t getNChannelsToDisplay() {
-        return this->n_channels_to_show;
+        return _n_channels_to_show;
     };
     int64_t getDataChannelFromDisplayChannel(int display_channel) {
-        return this->which_channels_to_show[display_channel];
+        return _which_channels_to_show[display_channel];
     }
 
     void setScrollBarPosition(int64_t position) {
 
         //Error checking
-        if ((position < 0) || (position > this->n_channels_total)) {
+        if ((position < 0) || (position > _n_channels_total)) {
             std::cout
                 << "Scroll bar position received by AnalogRenderYAxis doesn't make sense"
                 << std::endl;
         }
 
-        this->scroll_bar_position = position;
+        _scroll_bar_position = position;
         calculate_which_channels_to_show();
     };
 
@@ -84,48 +84,48 @@ private:
 
     void calculate_line_offsets() {
 
-        this->line_offsets = std::vector<float>(this->n_channels_to_show);
+        _line_offsets = std::vector<float>(_n_channels_to_show);
 
-        float spacing = 1.0 / (this->line_offsets.size() + 1);
-        for (int x = 0; x < this->line_offsets.size(); x++) {
-            line_offsets[x] = spacing * (x + 1);
+        float spacing = 1.0 / (_line_offsets.size() + 1);
+        for (int x = 0; x < _line_offsets.size(); x++) {
+            _line_offsets[x] = spacing * (x + 1);
         }
     };
 
     void calculate_channel_bounds() {
 
-        this->lower_channel_bounds = std::vector<float>(this->n_channels_to_show);
+        _lower_channel_bounds = std::vector<float>(_n_channels_to_show);
 
-        float spacing = 1.0 / static_cast<float>(this->lower_channel_bounds.size());
-        for (int x = 0; x < this->lower_channel_bounds.size(); x++) {
-            lower_channel_bounds[x] = spacing * static_cast<float>(x + 1);
+        float spacing = 1.0 / static_cast<float>(_lower_channel_bounds.size());
+        for (int x = 0; x < _lower_channel_bounds.size(); x++) {
+            _lower_channel_bounds[x] = spacing * static_cast<float>(x + 1);
         }
     };
 
     void calculate_which_channels_to_show() {
 
-        this->which_channels_to_show = std::vector<int64_t>(this->n_channels_to_show,0);
+        _which_channels_to_show = std::vector<int64_t>(_n_channels_to_show, 0);
 
-        int start_channel = this->scroll_bar_position;
+        int start_channel = _scroll_bar_position;
 
-        if ((start_channel + this->n_channels_to_show) > this->n_channels_total) {
-            start_channel = this->n_channels_total - this->n_channels_to_show;
+        if ((start_channel + _n_channels_to_show) > _n_channels_total) {
+            start_channel = _n_channels_total - _n_channels_to_show;
         }
 
-        for (int i = 0; i < this->n_channels_to_show; i++) {
-            this->which_channels_to_show[i] = start_channel + i - 1;
+        for (int i = 0; i < _n_channels_to_show; i++) {
+            _which_channels_to_show[i] = start_channel + i - 1;
         }
     };
 
-    int64_t n_channels_total;
-    int64_t n_channels_to_show;
+    int64_t _n_channels_total;
+    int64_t _n_channels_to_show;
 
-    int64_t scroll_bar_position;
+    int64_t _scroll_bar_position;
 
-    std::vector<int64_t> which_channels_to_show;
-    std::vector<float> per_channel_gain;
-    std::vector<float> line_offsets;
-    std::vector<float> lower_channel_bounds;
+    std::vector<int64_t> _which_channels_to_show;
+    std::vector<float> _per_channel_gain;
+    std::vector<float> _line_offsets;
+    std::vector<float> _lower_channel_bounds;
 };
 
 #endif // ANALOGRENDERYAXISPROPS_HPP
